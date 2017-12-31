d3bb80b3d54c36fb5935923509ba892fbcf9f98a
Listbd <brianl@csgpro.com>
2017-12-30 19:39:15 -0800

    move functions to static service

--- ClientApp/components/projects/projects.ts
+++ ClientApp/components/projects/projects.ts
@@ -27,61 +27,24 @@ export default class ProjectsComponent extends Vue {
     }
 
     mounted() {
-        let auth = btoa(`test:test`);
-        let headers = { 'Authorization': 'Basic ' + auth };
-        let url = TimeTrackerService.url + 'projects';
-        //let url = 'api/SampleData/Projects';
-
-        // -----------------------------------------------------------------
-        // NOTE: Apparently, the API at b-timeback.azurewebsites.net does not camel case 
-        // the returned properties. It keeps them Pascal. So, that appears to be a function
-        // of the backend API and not the front end interpretation, as I had perhaps
-        // previously thought
-        // -----------------------------------------------------------------
-
-        // Get ERROR does not pass Access-Control-Allow-Origin, CORS issues?
-        // this.projects = TimeTrackerService.fetchProjects(); // from TimeTrackerService
-
-        //fetch(url, { headers: headers })
-        //    .then(response => response.json() as Promise<Project[]>)
-        //    .then(data => {
-        //        // data is coming back Pascal case from the b-time api
-        //        this.projects = data;
-        //    });
-
-        axios.get(url, { headers: headers })
-            .then(response => {
-                debugger;
-                // data is coming back Pascal case from the b-time api
-                this.projects = response.data;
-            });
-
+        TimeTrackerService.getProjects().then(response => {
+            this.projects = response.data;
+        }).catch(error => {
+            debugger;
+        });
     }
 
     addProject() {
-        //this.projects.push(this.blankProject);
-
-        let auth = btoa(`test:test`);
-        let headers = { 'Authorization': 'Basic ' + auth };
-        let url = TimeTrackerService.url + "projects?format=json&callId=" + TimeTrackerService.generateGuid();
-        axios.post(url, this.blankProject, { headers: headers })
-            .then(response => {
-                let newProject = response.data;
-                // it doesn't have tasks yet so give it a blank array
-                newProject.ProjectTasks = [];
-                this.projects.push(newProject);
-
-                this.initializeBlankProject();
-                // refetch projects
-                //axios.get(TimeTrackerService.url + 'projects', { headers: headers })
-                //    .then(response2 => {
-                //        // data is coming back Pascal case from the b-time api
-                //        this.projects = response2.data;
-                //    });
-            }).catch(error => {
-                // TODO - toastr or something
-                console.log(error.response);
-            });
+        TimeTrackerService.addProject(this.blankProject).then(response => {
+            let newProject = response.data;
+            // it doesn't have tasks yet so give it a blank array
+            newProject.ProjectTasks = [];
+            this.projects.push(newProject);
+            this.initializeBlankProject();
+        }).catch(error => {
+            // TODO - toastr or something
+            console.log(error.response);
+        });
     }
 
     // ARROW FUNCTION DOES NOT WORK, IT DOES NOT CORRECTLY DETERMINE WHAT THIS IS
@@ -95,20 +58,13 @@ export default class ProjectsComponent extends Vue {
     // read property of undefined or Uncaught TypeError: this.myMethod is not a function.
 
     deleteProject(project: Project, event: any) {
-        //if (this.projects.length > 0) {
-        //    this.projects.pop();
-        //}
-        let url = `${TimeTrackerService.url}projects/${project.ProjectId}?format=json&callId=${TimeTrackerService.generateGuid()}`;
-        let auth = btoa(`test:test`);
-        let headers = { 'Authorization': 'Basic ' + auth };
-        axios.delete(url, { headers: headers })
-            .then(response => {
-                // remove this one from the array
-                // vue doesn't have find, so use filter
-                this.projects = this.projects.filter(item => {
-                    if (item.ProjectId !== project.ProjectId) return item;
-                });
-            })
+        TimeTrackerService.deleteProject(project.ProjectId).then(response => {
+            // remove this one from the array
+            // vue doesn't have find, so use filter
+            this.projects = this.projects.filter(item => {
+                if (item.ProjectId !== project.ProjectId) return item;
+            });
+        })
             
 
 
--- ClientApp/components/shared/timeTrackerService.ts
+++ ClientApp/components/shared/timeTrackerService.ts
@@ -1,31 +1,41 @@
-﻿import { Project } from '../shared/interfaces/project';
+﻿import axios from 'axios';
+import { Project } from '../shared/interfaces/project';
 
 // TODO - this feels like a poor man's service implementation
 
 export abstract class TimeTrackerService {
     //static url: string = 'https://b-timeback.azurewebsites.net/api/';
-    static url: string = 'http://localhost:57214/api/';
+    static apiUrl: string = 'http://localhost:57214/api/';
 
-    static fetchProjects = () => {
-        let headers = { 'Authorization': 'Basic ' + btoa(`test:test`) };
-        let f = fetch(TimeTrackerService.url, { headers: headers })
-            .then(response => response.json() as Promise<Project[]>)
-            .then(data => {
-                return data;
-            }).catch(error => {
-                // TODO
-            });
-        if (f instanceof Array) return f;
-        else return [];
+    static getProjects() {
+        let auth = btoa(`test:test`);
+        let headers = { 'Authorization': 'Basic ' + auth };
+        let url = TimeTrackerService.apiUrl + 'projects';
+        let result: Project[] = [];
+        return axios.get(url, { headers: headers });
     }
 
-    static s4 = () => {
+    static addProject(project: Project) {
+        let auth = btoa(`test:test`);
+        let headers = { 'Authorization': 'Basic ' + auth };
+        let url = TimeTrackerService.apiUrl + "projects?format=json&callId=" + TimeTrackerService.generateGuid();
+        return axios.post(url, project, { headers: headers });
+    }
+
+    static deleteProject(projectId: number) {
+        let url = `${TimeTrackerService.apiUrl}projects/${projectId}?format=json&callId=${TimeTrackerService.generateGuid()}`;
+        let auth = btoa(`test:test`);
+        let headers = { 'Authorization': 'Basic ' + auth };
+        return axios.delete(url, { headers: headers });
+    }
+
+    static s4() {
         return Math.floor((1 + Math.random()) * 0x10000)
             .toString(16)
             .substring(1);
     }
 
-    static generateGuid = () => {
+    static generateGuid() {
         return TimeTrackerService.s4() + TimeTrackerService.s4() + '-' + TimeTrackerService.s4() + '-' + TimeTrackerService.s4() + '-' +
             TimeTrackerService.s4() + '-' + TimeTrackerService.s4() + TimeTrackerService.s4() + TimeTrackerService.s4();
     }

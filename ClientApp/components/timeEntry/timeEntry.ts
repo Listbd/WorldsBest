9c8c171881fc4a9379b0187faf22dbe808a0e1dc
Listbd <brianl@csgpro.com>
2018-01-10 11:14:27 -0800

    wip - time entry

--- ClientApp/components/timeEntry/timeEntry.ts
+++ ClientApp/components/timeEntry/timeEntry.ts
@@ -4,17 +4,23 @@ import { Project } from '../shared/interfaces/project';
 import { ProjectTask } from '../shared/interfaces/projectTask';
 import { TimeEntry } from '../shared/interfaces/timeEntry';
 //import * as toastr from 'toastr';
-import { Moment } from 'moment';
+import * as moment from 'moment';
 import { TimeTrackerService } from '../shared/timeTrackerService';
 
+export interface DayEntry {
+    title: string,
+    timeEntries: TimeEntry[]
+}
 
 @Component
 export default class TimeEntryComponent extends Vue {
     @Prop()
     id: string; // always comes as string anyway, haven't figure out how to get it as a number
 
-    timeEntries: TimeEntry[] = [];
+    dayEntries: DayEntry[] = [];
     blankTimeEntry: TimeEntry
+
+    projectOptions: Project[] = [];
 
     constructor() {
         super();
@@ -36,15 +42,34 @@ export default class TimeEntryComponent extends Vue {
     }
 
     mounted() {
+        // Get list of projects
+        TimeTrackerService.getProjects().then(response => {
+            this.projectOptions = response.data;
+        }).catch(error => {
+            debugger;
+        });
+
+        // Get time entries
         let daysToPull = 10;
         for (let i = 0; i < daysToPull; i++) {
-            TimeTrackerService.getTimeEntriesForDate('2018-01-09').then(response => {
-                this.timeEntries.push(response.data);
+            let dateFormatted = moment(Date.now()).subtract(i, 'days').format('YYYY-MM-DD');
+            TimeTrackerService.getTimeEntriesForDate(dateFormatted).then(response => {
+                debugger;
+                let dayEntry = {
+                    title: dateFormatted,
+                    timeEntries: response.data
+                }
+                this.dayEntries.push(dayEntry);
             }).catch(error => {
                 console.log(error.Message);
                 debugger;
             });
         }
+    }
+
+    updateTasks(data: any) {
+        alert(data.currentTarget.value);
+        debugger;
     }
 
 }
